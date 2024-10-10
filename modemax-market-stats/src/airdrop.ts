795f4ac43f8bd7336e69175acfad6e4bddc17fcc
Tinkler <tinkler@163.com>
2024-10-10 17:35:31 +0800

    feat: phase2

--- modemax-market-stats/src/airdrop.ts
+++ modemax-market-stats/src/airdrop.ts
@@ -11,8 +11,8 @@ import {_getDayId, _getEpochTimestampId} from "./helpers";
 
 let ZERO = BigInt.zero();
 let EPOCH = BigInt.fromI32(1)
-export let EPOCH_START_TIME = BigInt.fromI32(1725494400) // Start time：2024/09/10 02:00:00 PM (UTC)
-export let EPOCH_END_TIME = BigInt.fromI32(1728489600) // End time: 2024/10/10 02:00:00 PM (UTC)
+export let EPOCH_START_TIME = BigInt.fromI32(1728543600) // Start time：2024/10/10 07:00:00 PM (UTC)
+export let EPOCH_END_TIME = BigInt.fromI32(1731135600) // End time: 2024/11/09 07:00:00 PM (UTC)
 
 //lpDelta: is + or - value
 export function storeUserData(
